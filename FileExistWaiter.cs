7a26bc9f1db1f763bb561ad565d70d4b7d2a76f0
thines01 <thines01@yahoo.com>
2023-07-08 13:19:53 -0500

    Add project files.

---
+++ FileExistWaiter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace FileExistWaiter
+{
+   using Delay;
+
+   public class CFileExistWaiter
+   {
+      public static bool WaitForFile(string strFileName, uint uintSecondsToWait, uint numDelaySeconds, ref string strError)
+      {
+         bool blnRetVal = false;
+         uint uintMaxSeconds = 0;
+         try
+         {
+            while (uintMaxSeconds < uintSecondsToWait)
+            {
+               if (File.Exists(strFileName))
+               {
+                  blnRetVal = true;
+                  break;
+               }
+
+               CDelay.Delay(numDelaySeconds);
+               uintMaxSeconds += numDelaySeconds;
+            }
+         }
+         catch (Exception exc)
+         {
+            blnRetVal = false;
+            strError = exc.Message;
+         }
+
+         return blnRetVal;
+      }
+   }
+}
